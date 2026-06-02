89e04637840a8e8d9fb397e8f5d628ce338dd0c6
jverzani <jverzani@gmail.com>
2026-06-02 15:56:51 -0400

    add possible case

--- src/conversion.jl
+++ src/conversion.jl
@@ -532,6 +532,8 @@ function Base.convert(::Type{Float64}, g::GiacExpr)::Float64
     elseif t == FRAC
         r = _convert_to_rational(g)
         return Float64(r)
+    elseif is_constant(g)
+        float(to_julia(g))
     else
         # Symbolic constants, `sqrt(2)`, the non-finite atoms: `AbstractFloat`
         # handles these, and it would be odd for `convert` to refuse what
